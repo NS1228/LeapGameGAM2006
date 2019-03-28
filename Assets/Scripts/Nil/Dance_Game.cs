ba1d4e476a77fffb8a22fbccb03770e0d44db992
Unknown <ns1228yt@gmail.com>
2019-03-28 12:15:51 +0000

    github beginning

---
+++ Assets/Scripts/Nil/Dance_Game.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Dance_Game : MonoBehaviour
+{
+
+    private bool w1;
+    private bool w2;
+    private bool w3;
+
+    public Button m_YourFirstButton, m_YourSecondButton, m_YourThirdButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // m_YourFirstButton.onClick.AddListener(TaskOnClick);
+      //  m_YourSecondButton.onClick.AddListener(delegate { TaskWithParameters("Hello"); });
+        m_YourThirdButton.onClick.AddListener(() => ButtonClicked(42));
+        m_YourSecondButton.onClick.AddListener(() => ButtonClicked(41));
+        m_YourFirstButton.onClick.AddListener(() => ButtonClicked(40));
+        // m_YourThirdButton.onClick.AddListener(TaskOnClick);
+        w1 = true;
+        w2 = false;
+        w3 = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+    }
+
+    void TaskWithParameters(string message)
+    {
+        //Output this to console when the Button2 is clicked
+        Debug.Log(message);
+    }
+
+    void ButtonClicked(int buttonNo)
+    {
+        //Output this to console when the Button3 is clicked
+        //Debug.Log("Button clicked = " + buttonNo);
+
+        if (w1)
+        {
+            
+           
+        }
+      else
+        {
+            SceneManager.LoadScene("test");
+        }
+
+       
+
+    }
+
+    
+
+
+}
---
+++ Assets/Scripts/Nil/Dance_Test.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dance_Test : MonoBehaviour
+{
+
+    private int set;
+
+    public GameObject correctoncolor;
+    public GameObject correctoncolor2;
+    public GameObject correctoncolor3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //correctoncolor = GameObject.FindGameObjectWithTag("Pick");
+        set = 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tagchanger();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Pick")
+        {
+            print("correct");
+            set++;
+            print(set);
+            
+        }
+        else
+        {
+            SceneManager.LoadScene("test");
+        }
+    }
+
+
+    private void Tagchanger()
+    {
+        if(set == 2)
+        {
+            correctoncolor.gameObject.tag = "RNG";
+            correctoncolor2.gameObject.tag = "Pick";
+            correctoncolor3.gameObject.tag = "RNG";
+        }
+        if (set == 3)
+        {
+            correctoncolor.gameObject.tag = "RNG";
+            correctoncolor2.gameObject.tag = "RNG";
+            correctoncolor3.gameObject.tag = "Pick";
+        }
+        
+    }
+
+
+}
