2ce81d1ef39cf59e37ef95ab3365cdfbaf103db5
Unknown <lb1055@live.mdx.ac.uk>
2019-03-31 21:03:20 +0100

    Cup Scripts

---
+++ Assets/Scripts/Lemar/Cup.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cup : MonoBehaviour {
+    public float downH = 2f;
+    public float upH = 3f;
+    public GameObject ball;
+    public Vector3 targetposition;
+
+    // Use this for initialization
+    void Start () {
+        targetposition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, targetposition, Time.deltaTime);
+
+        if (ball != null)
+        {
+            ball.transform.position = new Vector3
+                (
+                transform.position.x,
+                ball.transform.position.y,
+                transform.position.z
+                );
+        }
+	}
+
+    public void Moveup()
+    {
+        targetposition = new Vector3
+            (
+          transform.position.x,
+          upH,
+          transform.position.z
+            );
+    }
+    public void MoveDown()
+    {
+        targetposition = new Vector3
+            (
+          transform.position.x,
+          downH,
+          transform.position.z
+            );
+    }
+}
---
+++ Assets/Scripts/Lemar/Moving.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public GameObject ball;
+    public Cup[] cups;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(ShuffleRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private IEnumerator ShuffleRoutine()
+    {
+
+        yield return new WaitForSeconds(2f);
+
+        foreach (Cup cup in cups)
+        {
+            cup.Moveup();
+        }
+        yield return new WaitForSeconds(2f);
+
+        Cup targetcup = cups[Random.Range(0, cups.Length)];
+        targetcup.ball = ball;
+        ball.transform.position = new Vector3(
+            targetcup.transform.position.x,
+            ball.transform.position.y,
+           targetcup.transform.position.z);
+
+        yield return new WaitForSeconds(2f);
+
+        foreach (Cup cup in cups)
+        {
+            cup.MoveDown();
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Cup cup1 = cups[Random.Range(0, cups.Length)];
+            Cup cup2 = cup1;
+
+            while (cup2 == cup1)
+            {
+                cup2 = cups[Random.Range(0, cups.Length)];
+            }
+            Vector3 cup1Position = cup1.targetposition;
+            cup1.targetposition = cup2.targetposition;
+            cup2.targetposition = cup1Position;
+
+            yield return new WaitForSeconds(1f);
+
+        }
+    }
+}
