60d23c464e57245aa1744ef0753a1c25e768df88
Unknown <ns1228yt@gmail.com>
2019-04-02 22:50:07 +0100

    Buttons for dance game

---
+++ Assets/Scripts/Nil/Button_Test.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_Test : MonoBehaviour
+{
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+public void Ballone()
+    {
+        if (Dance_Sets.SETS == 1 || Dance_Sets.SETS == 4 || Dance_Sets.SETS == 7)
+        {
+            print("CORRECT");
+            Dance_Sets.SETS++;
+            Debug.Log(Dance_Sets.SETS);
+        }
+        else
+        {
+
+            Debug.Log("WRONG");
+            SceneManager.LoadScene("Dancegame");
+        }
+    }
+
+    public void BallTwo()
+    {
+        if (Dance_Sets.SETS == 2 || Dance_Sets.SETS == 5 || Dance_Sets.SETS == 8)
+        {
+            print("CORRECT");
+            Dance_Sets.SETS++;
+            Debug.Log(Dance_Sets.SETS);
+        }
+        else
+        {
+            Debug.Log("WRONG");
+            SceneManager.LoadScene("Dancegame");
+        }
+    }
+
+    public void BallThree()
+    {
+        if (Dance_Sets.SETS == 3 || Dance_Sets.SETS == 6 || Dance_Sets.SETS == 9)
+        {
+            print("CORRECT");
+            Dance_Sets.SETS++;
+            Debug.Log(Dance_Sets.SETS);
+        }
+        else
+        {
+            Debug.Log("WRONG");
+            SceneManager.LoadScene("Dancegame");
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Nil/Dance_Sets.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dance_Sets : MonoBehaviour
+{
+
+    public static int SETS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SETS = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
