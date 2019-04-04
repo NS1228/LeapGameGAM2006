18111f50a76cb779f71e591e5a974fba8e0448fc
Unknown <ns1228yt@gmail.com>
2019-04-04 21:28:25 +0100

    Dance Game (UPDATED 1.1)

--- Assets/Scripts/Nil/Button_Test.cs
+++ Assets/Scripts/Nil/Button_Test.cs
@@ -6,64 +6,217 @@ using UnityEngine.SceneManagement;
 public class Button_Test : MonoBehaviour
 {
 
-   
+
     // Start is called before the first frame update
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-public void Ballone()
-    {
-        if (Dance_Sets.SETS == 1 || Dance_Sets.SETS == 4 || Dance_Sets.SETS == 7)
-        {
-            print("CORRECT");
-            Dance_Sets.SETS++;
-            Debug.Log(Dance_Sets.SETS);
-        }
-        else
-        {
 
-            Debug.Log("WRONG");
-            SceneManager.LoadScene("Dancegame");
+    }
+    public void Ballone()
+    {
+        if (Dance_Sets.ROUNDS == 1)
+        {
+            if (Dance_Sets.SETS == 1 || Dance_Sets.SETS == 3)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
+        }
+
+        if (Dance_Sets.ROUNDS == 2)
+        {
+            if (Dance_Sets.SETS == 2 || Dance_Sets.SETS == 6)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
+        }
+
+        if (Dance_Sets.ROUNDS == 3)
+        {
+            if (Dance_Sets.SETS == 3 || Dance_Sets.SETS == 5 || Dance_Sets.SETS == 9)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
         }
     }
 
     public void BallTwo()
     {
-        if (Dance_Sets.SETS == 2 || Dance_Sets.SETS == 5 || Dance_Sets.SETS == 8)
+        if (Dance_Sets.ROUNDS == 1)
         {
-            print("CORRECT");
-            Dance_Sets.SETS++;
-            Debug.Log(Dance_Sets.SETS);
+            if (Dance_Sets.SETS == 2)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
         }
-        else
+
+        if (Dance_Sets.ROUNDS == 2)
         {
-            Debug.Log("WRONG");
-            SceneManager.LoadScene("Dancegame");
+            if (Dance_Sets.SETS == 4)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
+        }
+
+
+        if (Dance_Sets.ROUNDS == 3)
+        {
+            if (Dance_Sets.SETS == 1 || Dance_Sets.SETS == 4 || Dance_Sets.SETS == 8)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
         }
     }
 
     public void BallThree()
     {
-        if (Dance_Sets.SETS == 3 || Dance_Sets.SETS == 6 || Dance_Sets.SETS == 9)
+        if (Dance_Sets.ROUNDS == 1)
         {
-            print("CORRECT");
-            Dance_Sets.SETS++;
-            Debug.Log(Dance_Sets.SETS);
+            if (Dance_Sets.SETS == 5)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
         }
-        else
+
+        if (Dance_Sets.ROUNDS == 2)
         {
-            Debug.Log("WRONG");
-            SceneManager.LoadScene("Dancegame");
+            if (Dance_Sets.SETS == 3 || Dance_Sets.SETS == 5)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
+        }
+
+        if (Dance_Sets.ROUNDS == 3)
+        {
+            if (Dance_Sets.SETS == 2 || Dance_Sets.SETS == 6)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
         }
     }
 
+    public void BallFour()
+    {
+        if (Dance_Sets.ROUNDS == 1)
+        {
+            if (Dance_Sets.SETS == 4)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
+        }
 
+        if (Dance_Sets.ROUNDS == 2)
+        {
+            if (Dance_Sets.SETS == 1 || Dance_Sets.SETS == 7)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
 
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
+        }
+
+        if (Dance_Sets.ROUNDS == 3)
+        {
+            if (Dance_Sets.SETS == 7)
+            {
+                print("CORRECT");
+                Dance_Sets.SETS++;
+                Debug.Log(Dance_Sets.SETS);
+            }
+            else
+            {
+
+                Debug.Log("WRONG");
+                SceneManager.LoadScene("Dancegame");
+            }
+        }
+    }
 }
--- Assets/Scripts/Nil/Dance_Sets.cs
+++ Assets/Scripts/Nil/Dance_Sets.cs
@@ -6,16 +6,56 @@ public class Dance_Sets : MonoBehaviour
 {
 
     public static int SETS;
+    public static int ROUNDS;
 
     // Start is called before the first frame update
     void Start()
     {
         SETS = 1;
+        ROUNDS = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Roundsetter();
     }
+
+
+    public void Roundsetter()
+    {
+        if (ROUNDS == 1)
+        {
+            if (SETS == 6)
+            {
+                print("VICTORY... NEXT LEVEL");
+                ROUNDS++;
+                SETS = 1;
+                Debug.Log(ROUNDS);
+            }
+        }
+
+        if (ROUNDS == 2)
+        {
+            if (SETS == 8)
+            {
+                print("VICTORY... NEXT LEVEL");
+                ROUNDS++;
+                SETS = 1;
+                Debug.Log(ROUNDS);
+            }
+        }
+        if (ROUNDS == 3)
+        {
+            if (SETS == 10)
+            {
+                print("YOU WIN!");             
+            }
+        }
+    }
+
+
+
+
 }
+
