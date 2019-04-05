d21d9eb7d1622beddbbfd16e467cc6007798d851
Unknown <ns1228yt@gmail.com>
2019-04-05 21:08:41 +0100

    Dance Game (1.2 - Animations)

---
+++ Assets/Scripts/Nil/Bear_anims.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear_anims : MonoBehaviour
+{
+
+    
+    public GameObject Buttons;
+
+    public AnimationClip lvl1;
+    public AnimationClip lvl2;
+    //public AnimationClip lvl3;
+
+    Animation anim;
+
+
+    private bool animswitch;
+    // Start is called before the first frame update
+    void Start()
+    {
+      anim = gameObject.GetComponent<Animation>();
+
+        animswitch = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animplayer();   
+    }
+
+    public void Animplayer()
+    {
+        if (animswitch)
+        {
+            if (Dance_Sets.ROUNDS == 1)
+            {
+                anim.clip = lvl1;
+                anim.Play();
+                animswitch = false;
+            }
+        }
+        if (Dance_Sets.ROUNDS == 2)
+        {
+           
+            anim.clip = lvl2;
+            anim.Play();
+        }
+        if (Dance_Sets.ROUNDS == 3)
+        {
+            
+            //anim.clip = lvl3;
+            //anim.Play();
+        }
+    }
+}
--- Assets/Scripts/Nil/Dance_Sets.cs
+++ Assets/Scripts/Nil/Dance_Sets.cs
@@ -8,11 +8,18 @@ public class Dance_Sets : MonoBehaviour
     public static int SETS;
     public static int ROUNDS;
 
+    public GameObject buttons;
+
+ 
+
+    private Animation anim;
+
     // Start is called before the first frame update
     void Start()
     {
         SETS = 1;
         ROUNDS = 1;
+
     }
 
     // Update is called once per frame
@@ -26,6 +33,7 @@ public class Dance_Sets : MonoBehaviour
     {
         if (ROUNDS == 1)
         {
+            
             if (SETS == 6)
             {
                 print("VICTORY... NEXT LEVEL");
@@ -37,6 +45,7 @@ public class Dance_Sets : MonoBehaviour
 
         if (ROUNDS == 2)
         {
+            
             if (SETS == 8)
             {
                 print("VICTORY... NEXT LEVEL");
@@ -47,6 +56,7 @@ public class Dance_Sets : MonoBehaviour
         }
         if (ROUNDS == 3)
         {
+            
             if (SETS == 10)
             {
                 print("YOU WIN!");             
