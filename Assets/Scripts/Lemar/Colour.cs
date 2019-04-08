1ed9b1b4a66d4ef4f9d54f60a31cd64f0499d27d
Unknown <lb1055@live.mdx.ac.uk>
2019-04-08 22:20:06 +0100

    Colour Change

---
+++ Assets/Scripts/Lemar/Colour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colour : MonoBehaviour
+     
+{
+    public Material Red;
+    public Material Pink;
+    public GameObject Cup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            Cup.GetComponent<MeshRenderer>().material = Red;
+
+        }
+        if (other.gameObject.tag == "Floor")
+        {
+            Cup.GetComponent<MeshRenderer>().material = Pink;
+
+        }
+    }
+}
