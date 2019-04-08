9e062ade8f7a8b03a6b6dad996e7470a53d484e0
Unknown <lb1055@live.mdx.ac.uk>
2019-04-08 21:02:01 +0100

    Triangle Shape

--- Assets/Scripts/Lemar/GameWin.cs
+++ Assets/Scripts/Lemar/GameWin.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GameWin : MonoBehaviour
 {
+    public GameObject LoseBox;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoseBox.SetActive(true);
     }
 
     // Update is called once per frame
@@ -20,6 +21,7 @@ public class GameWin : MonoBehaviour
         if (other.gameObject.tag == "End")
         {
             print("You Win!");
+            LoseBox.SetActive(false);
             
         }
     }
