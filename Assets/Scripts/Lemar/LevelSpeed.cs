98e5ea4e0f9496863740a6fb9129de4ca25e337f
Unknown <lb1055@live.mdx.ac.uk>
2019-04-17 22:20:36 +0100

    Cup game UI/Scenes (finished)

--- Assets/Scripts/Lemar/LevelSpeed.cs
+++ Assets/Scripts/Lemar/LevelSpeed.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSpeed : MonoBehaviour
 {
     public static int Speed = 1;
+    public Text SpeedT;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +17,26 @@ public class LevelSpeed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SpeedT.text = "Speed: " + Speed + "/3";
     }
-    public void Win() {
-        SceneManager.LoadScene(2);
+    public void Win()
+    {
+        SceneManager.LoadScene(4);
         Speed++;
+        if (Speed == 4)
+        {
+            SceneManager.LoadScene(6);
+            Speed = 1;
+        }
+
     }
     public void Lose()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(4);
         Speed--;
-        if (Speed == 0) {
+        if (Speed == 0)
+        {
+            SceneManager.LoadScene(5);
             Speed = 1;
         }
     }
