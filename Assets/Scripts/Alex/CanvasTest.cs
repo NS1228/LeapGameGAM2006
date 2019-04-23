726d80ff01ec8145084d85e79f9a82e409b5ac07
Alex <alex.keoghly@outlook.com>
2019-04-23 10:02:50 +0100

    BarbieGame 0.3
    Fixed Retry Button

---
+++ Assets/Scripts/Alex/CanvasTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasTest : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject PauseCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player.SetActive(false);
+        PauseCamera.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
---
+++ Assets/Scripts/Alex/LoseScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScript : MonoBehaviour
+{
+
+    public GameObject BarbieInRoom;
+    public GameObject Mine1;
+    public GameObject Mine2;
+    public GameObject Canvas;
+    public GameObject TimerCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RetrySceneTrigger();
+    }
+
+    void RetrySceneTrigger()
+    {
+
+        if (Mine1 == null && Mine2 == null && BarbieInRoom.activeInHierarchy == true)
+        {
+            TimerCanvas.SetActive(false);
+            Canvas.SetActive(true);
+
+        }
+
+    }
+
+}
