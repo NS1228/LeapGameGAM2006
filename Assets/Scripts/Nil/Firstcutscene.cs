393a88775c6d7e7ebe135f6837aab03e83a217a0
Unknown <ns1228yt@gmail.com>
2019-04-25 20:04:03 +0100

    Animations

---
+++ Assets/Scripts/Nil/Firstcutscene.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Firstcutscene : MonoBehaviour
+{
+
+    private float timingsceneon;
+    private bool okay = false;
+    private bool lel = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timingsceneon = 13f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animscene();
+        Firstpart();
+    }
+
+    void Animscene ()
+    {
+        if(okay)
+        {
+            timingsceneon = 5000;
+            Nextpart();
+        }
+        
+    }
+
+    void Firstpart()
+    {
+        if (lel)
+        {
+            if (Time.time >= timingsceneon)
+            {
+
+                okay = true;
+                lel = false;
+                SceneManager.LoadScene("Scenetwo");
+            }
+        }
+    }
+    
+
+
+    void Nextpart()
+    {
+        if (Time.time <= timingsceneon)
+        {
+            SceneManager.LoadScene("Scenetwo");
+        }
+    }
+}
---
+++ Assets/Secondcutscene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Secondcutscene : MonoBehaviour
+{
+    public float timingscenetwo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //timingscenetwo = 13f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timingscenetwo)
+        {
+            SceneManager.LoadScene("Babygame");
+        }
+    }
+}
